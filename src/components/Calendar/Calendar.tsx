d32be02ca24ed53dfd819fdb70a8d54b4c1177fa
rmuti56 <rmuti56@hotmail.com>
2020-05-06 21:53:27 +0700

    conntect redux

---
+++ src/components/Calendar/Calendar.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect } from "react";
+import { connect, ConnectedProps } from "react-redux";
+
+import { RootState } from "../../redux/store";
+import {
+  selectUserEventsArray,
+  loadUserEvents,
+  UserEvent,
+} from "../../redux/user-events";
+import { addZero } from "../../helpers";
+
+const mapState = (state: RootState) => ({
+  events: selectUserEventsArray(state),
+});
+
+const mapDispatch = {
+  loadUserEvents,
+};
+
+const connector = connect(mapState, mapDispatch);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+interface Props extends PropsFromRedux {}
+
+const createDateKey = (date: Date) => {
+  const year = date.getUTCFullYear();
+  const month = date.getUTCMonth() + 1;
+  const day = date.getUTCDate();
+  return `${year}-${addZero(month)}-${addZero(day)}`;
+};
+
+const groupEventsByDay = (events: UserEvent[]) => {
+  const groups: Record<string, UserEvent[]> = {};
+
+  const addToGroup = (dateKey: string, event: UserEvent) => {
+    if (groups[dateKey] === undefined) {
+      groups[dateKey] = [];
+    }
+    groups[dateKey].push(event);
+  };
+  events.forEach((event) => {
+    const dateStartKey = createDateKey(new Date(event.dateStart));
+    const dateEndKey = createDateKey(new Date(event.dateEnd));
+    addToGroup(dateStartKey, event);
+    if (dateEndKey !== dateStartKey) {
+      addToGroup(dateEndKey, event);
+    }
+  });
+  return groups;
+};
+
+const Calendar: React.FC<Props> = ({ events, loadUserEvents }) => {
+  useEffect(() => {
+    loadUserEvents();
+  }, [loadUserEvents]);
+
+  let groupedEvents: ReturnType<typeof groupEventsByDay> | undefined;
+  let sortedGroupKeys: string[] | undefined;
+
+  if (events.length) {
+    groupedEvents = groupEventsByDay(events);
+    sortedGroupKeys = Object.keys(groupedEvents).sort(
+      (date1, date2) => +new Date(date1) - +new Date(date2)
+    );
+  }
+
+  return groupedEvents && sortedGroupKeys ? (
+    <>
+      {sortedGroupKeys.map((day) => {
+        // const events = groupedEvents![day];
+        return (
+          <div className="calendar" key={day}>
+            {day}
+            {/* <span>1 February</span>
+            <div>{events}</div> */}
+          </div>
+        );
+      })}
+    </>
+  ) : (
+    <div>loading...</div>
+  );
+};
+
+export default connector(Calendar);
--- src/components/Recorder/Recorder.tsx
+++ src/components/Recorder/Recorder.tsx
@@ -1,27 +1,13 @@
 import React, { useRef, useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { start, selectDateStart } from "../../redux/recorder";
+import { start, selectDateStart, stop } from "../../redux/recorder";
 
 const Recorder = () => {
   const dispatch = useDispatch();
   const dateStart = useSelector(selectDateStart);
   const started = dateStart !== "";
   let interval = useRef<number>(0);
-  const [count, setCount] = useState<number>(0);
-
-  const handleClick = () => {
-    dispatch(start());
-    interval.current = window.setInterval(() => {
-      setCount((prevCount) => prevCount + 1);
-    }, 1000);
-  };
-
-  useEffect(() => {
-    return () => {
-      window.clearInterval(interval.current);
-    };
-  }, []);
-
+  const [, setCount] = useState<number>(0);
   let secounds = started
     ? Math.floor((Date.now() - new Date(dateStart).getTime()) / 1000)
     : 0;
@@ -29,6 +15,24 @@ const Recorder = () => {
   secounds -= hours * 60 * 60;
   const minutes = secounds ? Math.floor(secounds / 60) : 0;
   secounds -= minutes * 60;
+
+  const handleClick = () => {
+    if (secounds) {
+      window.clearInterval(interval.current);
+      dispatch(stop());
+    } else {
+      dispatch(start());
+      interval.current = window.setInterval(() => {
+        setCount((prevCount) => prevCount + 1);
+      }, 1000);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      window.clearInterval(interval.current);
+    };
+  }, []);
 
   return (
     <div>
